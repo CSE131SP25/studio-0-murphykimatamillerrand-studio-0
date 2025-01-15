b0302ae3226a5b86187de69a5e70e169f311d2be
itzma <itzma@Mauz.dhcp.wustl.edu>
2025-01-15 15:14:08 -0600

    x

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" Hello, we have finished Studio 0 ");
+		System.out.println(" changes");
 	}   
 }
